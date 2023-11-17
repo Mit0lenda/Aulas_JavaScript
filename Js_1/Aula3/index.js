ce77be6bc951e0725e43b12b35662d79522724e1
Nicollas De Oliveira <113867356+Mit0lenda@users.noreply.github.com>
2023-11-17 12:46:16 -0300

    att

---
+++ Js_1/Aula3/index.js
@@ -0,0 +1,11 @@
+const idade = prompt("Informe sua Idade: ")
+if( idade > 18) {
+    alert("Maior de idade")
+} 
+else if(idade > 12 ){
+    alert("Menor de idade")
+} else if (idade > 4 ){
+    alert("Você é criança")
+} else {
+    alert("..")
+}
