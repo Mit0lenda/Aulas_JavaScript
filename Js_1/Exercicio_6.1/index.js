c2322cd18d6eaab6ab29f19fb00f9b3676d8938f
Nicollas De Oliveira <nideoliveirafreitas@gmail.com>
2024-03-04 16:16:47 -0300

    att

--- Js_1/Exercicio_6.1/index.js
+++ Js_1/Exercicio_6.1/index.js
@@ -1,1 +1,12 @@
-const palin =prompt("Qual a palavra")
+const palavra =prompt("What is the word ?")
+let palavrainvertida = ""
+for( let i = palavra.length - 1 ; i >= 0 ; i--)
+{
+    palavrainvertida += palavra[i]
+}
+if(palavra === palavrainvertida){
+    alert(palavra + "é um palíndromo!")
+}
+else{
+    alert(palavra + " não é um palíndromo!\n\n" + palavra + " !== " + palavrainvertida)
+}
