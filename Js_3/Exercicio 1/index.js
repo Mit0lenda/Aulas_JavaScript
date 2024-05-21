c9270ffb811cb45e17bfc162cfa0184630a808b4
Nicollas De Oliveira <nideoliveirafreitas@gmail.com>
2024-05-21 15:31:36 -0300

    att

---
+++ Js_3/Exercicio 1/index.js
@@ -0,0 +1,30 @@
+function remover(){
+    const number = document.getElementById("numberToRemove").value
+    const playerToRemove = document.getElementById("player-" + number)
+
+    const confirmation = confirm("Remover o jogador " + playerToRemove.innerText + "?")
+    if(confirmation){
+        document.getElementById("team-list").removeChild(playerToRemove)
+        //playerToRemove.remove()
+        document.getElementById("numberToRemove").value = ""
+    }
+}
+function escalar(){
+    const position = document.getElementById("position").value
+    const name = document.getElementById("name").value
+    const number = document.getElementById("number").value
+
+    const confirmation = confirm("Escalar " + name + " como " + position + "?")
+
+    if(confirmation){
+        const teamList = document.getElementById("team-list")
+        const playerItem = document.createElement("li")
+        playerItem.id = "player-" + number
+        playerItem.innerText = position + ": " + name + " (" + number + ")"
+        teamList.appendChild(playerItem)
+
+        document.getElementById("position").value = ""
+        document.getElementById("name").value = ""
+        document.getElementById("number").value = ""
+    }
+}
